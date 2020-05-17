3d6d9843cfc4fbf39e535229ad7508b6cae3ea46
xixi <lzx20181111@163.com>
2020-05-17 23:28:49 +0800

    add save dialog to use

---
+++ electronDemo10/main.js
@@ -0,0 +1,23 @@
+var electron = require('electron')
+
+var app = electron.app
+
+var BrowserWindow = electron.BrowserWindow
+
+var mainWindow = null
+
+app.on('ready', ()=>{
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 700,
+        webPreferences: {nodeIntegration: true}
+    })
+
+    mainWindow.webContents.openDevTools()
+
+    mainWindow.loadFile('index.html')
+
+    mainWindow.on('closed', ()=>{
+        mainWindow = null
+    })
+})
