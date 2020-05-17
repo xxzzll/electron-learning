792368e5604547c6a7eb477ed7217e044b72c5a2
xixi <lzx20181111@163.com>
2020-05-17 17:30:35 +0800

    modify main.js file

--- electronDemo01/main.js
+++ electronDemo01/main.js
@@ -1,12 +1,12 @@
 var electron = require('electron')
 
-var app = electron.app
-var BrowserWindow = electron.BrowserWindow
-var mainWindow = null
+var app = electron.app // 引入 app
+var BrowserWindow = electron.BrowserWindow // 引入 窗口
+var mainWindow = null // 声明要打开的 主窗口
 
 app.on('ready', ()=>{
     mainWindow = new BrowserWindow({width:300, height:400})
-    mainWindow.loadFile('index.html')
+    mainWindow.loadFile('index.html') // 加载 html 页面
     mainWindow.on('closed', ()=>{
         mainWindow = null
     })
