5d370e99b9b193fb17c24b1a3d6cd8a7e65fd1d3
xixi <lzx20181111@163.com>
2020-05-18 22:15:34 +0800

    add notification

---
+++ electronDemo13/man.js
@@ -0,0 +1,22 @@
+var electron = require('electron')
+
+var app = electron.app
+var BrowserWindow = electron.BrowserWindow
+
+var mainWindow = null
+
+app.on('ready', ()=>{
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 650,
+        webPreferences: {nodeIntegration: true}
+    })
+
+    mainWindow.webContents.openDevTools()
+
+    mainWindow.loadFile('index.html')
+
+    mainWindow.on('closed', ()=>{
+        mainWindow = null
+    })
+})
